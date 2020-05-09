31955fa2f431c76d9afb17412f40570d3fda2d63
Seltzer-Software-Services-LLC <64972986+Seltzer-Software-Services-LLC@users.noreply.github.com>
2020-05-09 19:21:42 -0400

    Add files via upload

---
+++ DailyTracker/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PRMNYReporting.DailyTracker.Win")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Global Affairs Canada / Affaires mondiales Canada")>
+<Assembly: AssemblyProduct("PRMNYReporting.DailyTracker.Win")>
+<Assembly: AssemblyCopyright("Copyright © Global Affairs Canada / Affaires mondiales Canada 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3e00d3ba-8fc7-4fd0-970e-36cee9aa78d7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
